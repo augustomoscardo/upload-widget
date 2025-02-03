fdc2efd93b0535a7cd73ab10e105721d49b77b67
Augusto Moscardo <augustomsaluz@gmail.com>
2025-02-02 22:29:46 -0300

    Setup Immer + zustand & upload file to server

--- web/src/store/uploads.ts
+++ web/src/store/uploads.ts
@@ -1,4 +1,7 @@
 import { create } from "zustand";
+import { enableMapSet } from 'immer'
+import {immer} from 'zustand/middleware/immer'
+import { uploadFileToStorage } from "../http/upload-file-to-storage";
 
 export type Upload = {
   name: string;
@@ -10,26 +13,41 @@ type UploadState = {
   addUploads: (files: File[]) => void;
 };
 
-export const useUploads = create<UploadState>((set, get) => {
-  function addUploads(files: File[]) {
-    for (const file of files) {
-      const uploadId = crypto.randomUUID();
+enableMapSet()
 
-      const upload: Upload = {
-        name: file.name,
-        file,
-      };
-      
-      set((state) => {
-        return {
-          uploads: state.uploads.set(uploadId, upload),
-        };
-      });
+export const useUploads = create<UploadState, [['zustand/immer', never]]>(
+  immer((set, get) => {
+    async function processUpload(uploadId: string) {
+      const upload = get().uploads.get(uploadId);
+
+      if (!upload) {
+        return;
+      }
+
+      await uploadFileToStorage({ file: upload.file });
     }
-  }
 
-  return {
-    uploads: new Map(),
-    addUploads,
-  };
-});
+    function addUploads(files: File[]) {
+      for (const file of files) {
+        const uploadId = crypto.randomUUID();
+
+        const upload: Upload = {
+          name: file.name,
+          file,
+        };
+        
+        set((state) => {
+          state.uploads.set(uploadId, upload);
+        });
+
+        processUpload(uploadId);
+      }
+    }
+
+    return {
+      uploads: new Map(),
+      addUploads,
+
+    };
+  })
+);
